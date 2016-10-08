264db400e59ea2e9aeae75d24b1dfeabe171d55a
Christopher Leighton <chle0996@colorado.edu>
2016-10-08 13:46:04 -0600

    Added sphere-rendering in prep for 3D simulations.

--- circle.m
+++ circle.m
@@ -6,9 +6,15 @@ function [] = circle(bodies,t)
         yunit=bodies(i).radius*sin(th)+bodies(i).position(2);
         plot(xunit,yunit);
     end
-    axis([-15 15 -15 15]); % Dimensions of system
+%     [x,y,z]=sphere; % Generate a sphere
+%     for i=1:length(bodies)
+%         x_pos=bodies(i).position(1);
+%         y_pos=bodies(i).position(2);
+%         surf(x*bodies(i).radius+x_pos,y*bodies(i).radius+y_pos,z*bodies(i).radius); % Plot the sphere
+%     end
+    
     title(['Time (s): ' num2str(t)]); % Display elapsed time
     
     pause(0.00001); % Required for plot() to briefly display current system
-    %clf('reset'); % Re-enable this to display past trajectories
+    cla; % Disable this to display past trajectories
 end
