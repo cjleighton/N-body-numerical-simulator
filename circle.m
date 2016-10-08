32f20875901b14c4019db11faaa7afd7ab4acad9
Christopher Leighton <chle0996@colorado.edu>
2016-10-07 23:59:31 -0600

    Added comments.

--- circle.m
+++ circle.m
@@ -1,16 +1,14 @@
-function [] = circle(bodies,r,t)
+function [] = circle(bodies,t)
     hold on;
     th = 0:pi/50:2*pi;
     for i=1:length(bodies)
-        xunit=r*cos(th)+bodies(i).position(1);
-        yunit=r*sin(th)+bodies(i).position(2);
+        xunit=bodies(i).radius*cos(th)+bodies(i).position(1);
+        yunit=bodies(i).radius*sin(th)+bodies(i).position(2);
         plot(xunit,yunit);
-        %text(bodies(i).position(1),bodies(i).position(2),[num2str(bodies(i).mass) ' kg']);
     end
+    axis([-15 15 -15 15]); % Dimensions of system
+    title(['Time (s): ' num2str(t)]); % Display elapsed time
     
-    axis([-30 30 -20 20]);
-    title(['Time (s): ' num2str(t)]); % display time elapsed
-    
-    pause(0.0000000000000000001);
-    clf('reset');
+    pause(0.00001); % Required for plot() to briefly display current system
+    %clf('reset'); % Re-enable this to display past trajectories
 end
